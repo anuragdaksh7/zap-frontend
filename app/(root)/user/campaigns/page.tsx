865d9772054a60af726c20d3657062b177c9d59c
Shivansh Agrawal <shivansh.agrprof@gmail.com>
2025-06-22 12:02:17 +0530

    fix: set initial state of upload dialog to closed

--- app/(root)/user/campaigns/page.tsx
+++ app/(root)/user/campaigns/page.tsx
@@ -1,10 +1,11 @@
+'use client'
 import React, { useState } from "react";
 import { CampaignsTable } from "@/components/CampaignsTable";
 import { PlusSquare } from "lucide-react";
 import { CSVDialog } from "@/components/CSVDialog";
 
 const page = () => {
-  const [isUploadDialogOpen, setIsUploadDialogOpen] = useState(true);
+  const [isUploadDialogOpen, setIsUploadDialogOpen] = useState(false);
   return (
     <div className="w-[85%] h-screen">
       <header className="w-full bg-white text-foreground border-b-[1px] px-6 pt-1 mb-8 h-[10vh] flex-between">
@@ -14,7 +15,7 @@ const page = () => {
             Checkout all your campaigns here.
           </p>
         </div>
-        <button className='bg-cta  px-3 py-2 text-lg transition-colors text-white hover:bg-ctaHover rounded-sm flex-center gap-1 '><PlusSquare/>Button</button>
+        <button className='bg-cta  px-3 py-2 text-lg transition-colors text-white hover:bg-ctaHover rounded-sm flex-center gap-1' onClick={() => setIsUploadDialogOpen(true)}><PlusSquare/>Button</button>
       </header>
       <div className="w-[81vw] max-h-[80vh] bg-white border-2 rounded-xl p-4 mx-6 overflow-y-auto">
         <CampaignsTable />
